ffa2b3209c76691159ddfd81b1271878b1f831b7
stekap000 <stekap000@gmail.com>
2024-06-19 23:25:45 +0200

    build update

--- src/build.bat
+++ src/build.bat
@@ -1,4 +1,5 @@
 @echo off
 
-gcc -Wall -Wextra -o ..\main.exe one_neuron_double_input.c
-gcc -Wall -Wextra -o ..\main.exe gates.c
+REM gcc -Wall -Wextra -o ..\main.exe one_neuron_double_input.c
+REM gcc -Wall -Wextra -o ..\main.exe gates.c
+gcc -Wall -Wextra -o ..\main.exe xor.c
