1c2fe71031bbe66ab166137180bb45d0afbd8a73
stekap000 <stekap000@gmail.com>
2024-06-21 01:45:54 +0200

    build update

--- src/build.bat
+++ src/build.bat
@@ -2,4 +2,5 @@
 
 REM gcc -Wall -Wextra -o ..\main.exe one_neuron_double_input.c
 REM gcc -Wall -Wextra -o ..\main.exe gates.c
-gcc -Wall -Wextra -o ..\main.exe xor.c
+REM gcc -Wall -Wextra -o ..\main.exe xor.c
+gcc -Wall -Wextra -o ..\main.exe neural.c
