52d0864431fcaf21f872e9d304b861cef0f53633
stekap000 <stekap000@gmail.com>
2024-06-21 02:33:37 +0200

    alloc, print, macro types

--- src/neural.c
+++ src/neural.c
@@ -2,6 +2,14 @@
 #include "neural.h"
 
 int main() {
+	Matrix a = matrix_alloc(3, 4);
 
+	a.elements[5] = 1.23;
+	a.elements[2] = 212.23;
+	a.elements[11] = 1.0922;
+	a.elements[8] = 11.23;
+
+	matrix_print(a);
+	
 	return 0;
 }
--- src/neural.h
+++ src/neural.h
@@ -1,6 +1,8 @@
 #ifndef NEURAL_H
 #define NEURAL_H
 
+#include <stdio.h> // For printing.
+
 #ifndef NEURAL_ASSERT
 #include <assert.h>
 #define NEURAL_ASSERT(cond) assert(cond)
@@ -11,12 +13,25 @@
 #define NEURAL_MALLOC malloc
 #endif
 
+#ifndef NEURAL_CALLOC
+#include <stdlib.h>
+#define NEURAL_CALLOC calloc
+#endif
+
 #define NEURAL_NOT_IMPLEMENTED(msg) NEURAL_ASSERT(!(msg))
+
+#ifdef NEURAL_USE_DOUBLE
+#define Neural_Real double
+#define NEURAL_REAL_PRINT_TYPE "%.4lf"
+#else
+#define Neural_Real float
+#define NEURAL_REAL_PRINT_TYPE "%.4f"
+#endif
 
 typedef struct {
 	int rows;
 	int cols;
-	float* elements;
+	Neural_Real* elements;
 } Matrix;
 
 Matrix matrix_alloc(int rows, int cols);
@@ -29,8 +44,11 @@ void matrix_print(Matrix a);
 #ifdef NEURAL_IMPLEMENTATION
 
 Matrix matrix_alloc(int rows, int cols) {
-	NEURAL_NOT_IMPLEMENTED("");
-	
+	return (Matrix) {
+		.rows = rows,
+		.cols = cols,
+		.elements = malloc(rows*cols*sizeof(Neural_Real))
+	};
 }
 
 void matrix_add(Matrix a, Matrix b, Matrix *result) {
@@ -42,7 +60,14 @@ void matrix_mul(Matrix a, Matrix b, Matrix *result) {
 }
 
 void matrix_print(Matrix a) {
-	NEURAL_NOT_IMPLEMENTED("");
+	printf("[\n");
+	for(int i = 0; i < a.rows; ++i) {
+		for(int j = 0; j < a.cols; ++j) {
+			printf("\t"NEURAL_REAL_PRINT_TYPE", ", a.elements[i*a.cols + j]);
+		}
+		printf("\n");
+	}
+	printf("]\n");
 }
 
 #endif // NEURAL_IMPLEMENTATION
